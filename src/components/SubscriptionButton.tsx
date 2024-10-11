b035712dc0573d5da22abae3ab06a5f8607ccfdd
Jasperino64 <jasperschen@gmail.com>
2024-10-11 17:34:41 -0400

    update subscription button variant

--- src/components/SubscriptionButton.tsx
+++ src/components/SubscriptionButton.tsx
@@ -19,7 +19,7 @@ const SubscriptionButton = (props: Props) => {
     }
   }
   return (
-    <Button disabled={loading} onClick={handleSubscription} variant="default">
+    <Button disabled={loading} onClick={handleSubscription} variant="secondary">
       {props.isPro ? "Manage Subscriptions" : "Get Pro"}
     </Button>
   )
